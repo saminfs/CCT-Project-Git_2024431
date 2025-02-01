4c4ff42eeb34e365da331d7d357d13cbc73e7a84
Eusha Ibne Nur Sami <saminfs9@gmail.com>
2025-02-01 10:54:05 +0000

    First commit

---
+++ src/eushagit2024431/EushaGit2024431.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eushagit2024431;
+
+/**
+ *
+ * @author samin
+ */
+public class EushaGit2024431 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+        public static void main(String[] args) {
+        // TODO code application logic here
+        int number=22;
+        String name="Sam";
+        double height= 7.11;
+        
+        System.out.println("Sami is : " + number + " years old, Identify as : " + name + " infront of everyone, Height: " + height + " feet.");
+}
+}
+    
